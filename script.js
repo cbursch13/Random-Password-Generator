a969e05fb00831a10384cd7e238e065b2e297288
cammichbursc@gmail.com <cammichbursc@gmail.com>
2023-11-13 00:21:18 -0600

    Created variables and wrote out the password function

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  let characters = " ";
-  
-  if(lowercase){
-    characters += "abcdefghijklmnopqrstuvwxyz"
-  }
 
-  if(uppercase){
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXY"
-  }
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharArray =  ["@", "#", "!", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", ":", ";", "<", ">", "?"];
 
-  if(numbers){
-    characters += "0123456789"
-  }
+ var resultArray = [];
+ var userArray = [];
 
-  if(specialchar){
-    characters += "!@#$%^&*()_+:"
-  }
+ upperCaseArray [1]
 
-  let passwordArray = [];
+ var numCharacter = prompt ("How many characters do you want between 8 and 128?");
+ var uppercase = confirm ("Do you want uppercase in your password?");
+ var lowercase = confirm ("Do you want lowercase on your password?");
+ var numbers = confirm ("Do you want numbers in your passsword?");
+ var special = confirm ("Do you want special character in your password?");
+
+if (uppercase){
+  resultArray = resultArray.concat(upperCaseArray);
+}
+
+if (lowercase){
+  resultArray = resultArray.concat(lowerCaseArray);
+}
+
+if (numbers){
+  resultArray = resultArray.concat(numberArray);
+}
+
+if (special){
+  resultArray = resultArray.concat(specialCharArray);
+}
+console.log(resultArray)
 
 
-  //* 1. Prompt the user for the password criteria
-  //*   a. Password lengh between 8 < 128
-  //*   b. Lowercase, Uppercase, Numbers, Special Characters
-  //* 2. Validate the input
-  //* 3. Generate password based on criteria
-  
-  //* 4. Display the generated password on the page
-  return "Password will go here!"
+for (var i = 0; i < numCharacter, i++) {
+
+  userArray.push (resultArray[Math.floor(Math.random() * resultArray.length)]);
+
+}
+
+return userArray.join("") ;
 }
 
 //* Write password to the #password input
